58f743da2e709970fd753f4fd1a42fcdbfba8e77
Łukasz Piłatowski <lukpila29@gmail.com>
2021-02-17 11:44:10 +0100

    fix blob include

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='depthai_mock',
-    version='0.0.2',
+    version='0.0.3',
     description='The tool that allows you to record and playback the DepthAI generated frames',
     long_description=io.open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
